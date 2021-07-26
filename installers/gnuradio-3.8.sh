dc7af6ae12acc181b610585b0dbdf587bd11b545
NeuroForLunch <neuro.for.lunch@gmail.com>
2021-07-26 11:35:50 -0700

    updates

---
+++ installers/gnuradio-3.8.sh
@@ -0,0 +1,40 @@
+ln -sF /media/sf_shared-storage/dotfiles_linux/settings/gnuradio/config-3.8.conf /home/nfl/.gnuradio/config.conf
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/blocks.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/blocks.tree.yml /usr/local/share/gnuradio/grc/blocks/blocks.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/qtgui.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/qtgui.tree.yml /usr/local/share/gnuradio/grc/blocks/qtgui.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/fosphor.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/fosphor.tree.yml /usr/local/share/gnuradio/grc/blocks/fosphor.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/fft.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/fft.tree.yml /usr/local/share/gnuradio/grc/blocks/fft.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/analog.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/analog.tree.yml /usr/local/share/gnuradio/grc/blocks/analog.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/channels.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/channels.tree.yml /usr/local/share/gnuradio/grc/blocks/channels.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/digital.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/digital.tree.yml /usr/local/share/gnuradio/grc/blocks/digital.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/dtv.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/dtv.tree.yml /usr/local/share/gnuradio/grc/blocks/dtv.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/fec.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/fec.tree.yml /usr/local/share/gnuradio/grc/blocks/fec.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/filter.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/filter.tree.yml /usr/local/share/gnuradio/grc/blocks/filter.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/grc.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/grc.tree.yml /usr/local/share/gnuradio/grc/blocks/grc.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/video.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/video.tree.yml /usr/local/share/gnuradio/grc/blocks/video.tree.yml
+
+rm -rf /usr/local/share/gnuradio/grc/blocks/vocoder.tree.yml
+ln -sF /media/sf_shared-storage/gnuradio-block-docs/documentation/tree_files/vocoder.tree.yml /usr/local/share/gnuradio/grc/blocks/vocoder.tree.yml
--- settings/gnuradio-3.9.sh
+++
@@ -1,1 +0,0 @@
-ln -sF /media/sf_shared-storage/dotfiles_linux/settings/gnuradio/config-3.9.conf /home/nfl/.gnuradio/config.conf
